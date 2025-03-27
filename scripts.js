027eba8e92bb9f7e23facad3072d43c2c697b831
TheRepeater77 <russchoolacc@gmail.com>
2025-03-27 15:33:30 +0800

    Number 5

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 const gallery = document.querySelector(".gallery");
 const cards = gallery.querySelectorAll(".card");
+const first_card = cards[0];
+const last_card = cards[cards.length-1];
 const pet_btns = gallery.querySelectorAll(".card button");
 const pet_all = document.querySelector("#btn-select-all");
 const unpet_all = document.querySelector("#btn-unselect-all");
 const toLast_btn = document.querySelector("#btn-select-last");
+const toFirst_btn = document.querySelector("#btn-select-first");
+
+let current_card = 0;
 
 /* ================ 1# Pet Button Heart START */
 pet_btns.forEach(button => {    
@@ -36,12 +41,30 @@ unpet_all.addEventListener("click",()=>{
 });
 /* Unpet All Button END ================ */
 
-/* ================ Last Button START */
+/* ================ To Last Button START */
 toLast_btn.addEventListener("click",()=>{
-    let last_card = cards[cards.length-1];
+    if(first_card.classList.contains("card-selected")){
+        first_card.classList.toggle("card-selected");
+        first_card.classList.toggle("active");   
+        current_card = 0;
+    }
     if(!last_card.classList.contains("card-selected")){
         last_card.classList.toggle("card-selected");
         last_card.classList.toggle("active");
     }
 });
 /* Last Button Button END ================ */
+
+/* ================ To First Button START */
+toFirst_btn.addEventListener("click",()=>{
+    if(!first_card.classList.contains("card-selected")){
+        first_card.classList.toggle("card-selected");
+        first_card.classList.toggle("active");
+    }
+    if(last_card.classList.contains("card-selected")){
+        last_card.classList.toggle("card-selected");
+        last_card.classList.toggle("active");
+        current_card = cards.length-1;
+    }
+});
+/* To First Button Button END ================ */
