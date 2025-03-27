1f3e3562bfdcea9ec4d66cfbef7b8ecd3f5e82c1
TheRepeater77 <russchoolacc@gmail.com>
2025-03-27 14:42:40 +0800

    Number 2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const pet_buttons = document.querySelectorAll(".gallery button");
+const pet_all = document.querySelector("#btn-select-all");
 
 /* ==================================================== 1# Pet Button Heart START */
 pet_buttons.forEach(button => {    
@@ -8,3 +9,14 @@ pet_buttons.forEach(button => {
     });
 });
 /* 1# Pet Button Heart END ==================================================== */
+
+/* ==================================================== Pet All Button START */
+pet_all.addEventListener("click",()=>{
+    pet_buttons.forEach((button) =>{
+        let heart = button.children[0];
+        if(!heart.classList.contains("fa-solid")){
+            heart.classList.toggle("fa-solid");
+        }
+    });
+});
+/* Pet All Button END ==================================================== */
